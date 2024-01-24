231011da5fd66ad001697c4cd72a3298b4392f06
Lukas Piwowarski <lpiwowar@redhat.com>
2024-01-24 06:35:24 -0500

    Ensure controllers can access persistentvolumeclaims
    This patch ensures that the controllers can access the persistent
    volume claims by updating the comments for kubebuilder.

--- controllers/tobiko_controller.go
+++ controllers/tobiko_controller.go
@@ -49,6 +49,7 @@ type TobikoReconciler struct {
 //+kubebuilder:rbac:groups=test.openstack.org,resources=tobikoes/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=test.openstack.org,resources=tobikoes/finalizers,verbs=update
 //+kubebuilder:rbac:groups="",resources=pods,verbs=create;delete;get;list;patch;update;watch
+//+kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;create;update;watch;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
