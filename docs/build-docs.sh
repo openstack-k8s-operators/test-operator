c4eaa5df15fc0b0ba74f11d14d572049cf3a2ca3
Jakub Skunda <jskunda@redhat.com>
2024-02-29 14:18:36 +0100

    test

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -12,9 +12,7 @@ pip install -c ${UPPER_CONSTRAINTS_FILE:-https://releases.openstack.org/constrai
 # Run linter on docs, skipping antsibull-docs output as it isn't up to spec
 doc8 --config ${DOCS_DIR}/doc8.ini ${DOCS_DIR}/source
 
-# Specify the output directory for the HTML files
-OUTPUT_DIR="/documentation"  # Change this to your desired output directory
-sphinx-build -a -E -W -d ${DOCS_DIR}/build/doctrees --keep-going -b html ${DOCS_DIR}/source ${OUTPUT_DIR} -T
+sphinx-build -a -E -W -d ${DOCS_DIR}/build/doctrees --keep-going -b html ${DOCS_DIR}/source ${DOCS_DIR}/build/html -T
 
 deactivate
 rm -fr ${TEMP_VENV_ENV}
