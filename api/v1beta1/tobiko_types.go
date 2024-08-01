ec08d8b88e3c9aa8ce9747e4363cc3a81a8efa4d
Katarina Strenkova <kstrenko@redhat.com>
2024-08-01 09:11:57 +0000

    Add debug mode for Tobiko
    Similar to debug mode for Tempest, this patch introduces new option
    for test-operator that keeps the pod running after a successful run
    or even after a failure has occured. It resolves the issue that
    oc rsh session is killed once the execution of the run_tobiko.sh
    file finishes.

--- api/v1beta1/tobiko_types.go
+++ api/v1beta1/tobiko_types.go
@@ -50,6 +50,14 @@ type TobikoSpec struct {
 	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 
 	// +kubebuilder:validation:Optional
+        // +kubebuilder:default=false
+        // Activate debug mode. When debug mode is activated any error encountered
+        // inside the test-pod causes that the pod will be kept alive indefinitely
+        // (stuck in "Running" phase) or until the corresponding Tobiko CR is deleted.
+        // This allows the user to debug any potential troubles with `oc rsh`.
+        Debug bool `json:"debug,omitempty"`
+
+	// +kubebuilder:validation:Optional
 	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	// This value contains a toleration that is applied to pods spawned by the
 	// test pods that are spawned by the test-operator.
