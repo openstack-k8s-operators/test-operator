b8d2880ec8bf3435f0aaff58bc80f33f8e75cbfd
Lukas Piwowarski <lpiwowar@redhat.com>
2024-08-15 07:23:52 -0400

    Enable RuntimeDefault SeccompProfile
    It's a good practice to run containers with RuntimeDefault SeccomProfile
    that allows only such a system calls that can be expected to be used
    during normal operation of the container.

--- pkg/horizontest/job.go
+++ pkg/horizontest/job.go
@@ -60,6 +60,9 @@ func Job(
 								Capabilities: &corev1.Capabilities{
 									Add: []corev1.Capability{"NET_ADMIN", "NET_RAW", "CAP_AUDIT_WRITE"},
 								},
+								SeccompProfile: &corev1.SeccompProfile{
+									Type: corev1.SeccompProfileTypeRuntimeDefault,
+								},
 							},
 						},
 					},
