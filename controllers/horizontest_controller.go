36f422f5d67cb981c3be4b30ae6499cfa605324c
Lukas Piwowarski <lpiwowar@redhat.com>
2024-09-27 08:30:44 -0400

    Fix lint issues
    This patch addresses two issues:
    
    - incorrect indentation in common_webhook.go
    - removal of automatically generated comments

--- controllers/horizontest_controller.go
+++ controllers/horizontest_controller.go
@@ -57,15 +57,7 @@ func (r *HorizonTestReconciler) GetLogger(ctx context.Context) logr.Logger {
 //+kubebuilder:rbac:groups="",resources=pods,verbs=create;delete;get;list;patch;update;watch
 //+kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;create;update;watch;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the HorizonTest object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
+// Reconcile - HorizonTest
 func (r *HorizonTestReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, _err error) {
 	Log := r.GetLogger(ctx)
 
