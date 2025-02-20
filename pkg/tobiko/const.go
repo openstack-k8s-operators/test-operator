22d263fc56cee7e943fd28b19537ae1616e99592
Katarina Strenkova <kstrenko@redhat.com>
2025-02-20 11:02:54 -0500

    Introduce ExtraMounts to test-operator
    This patch introduces the ExtraMounts parameter to test-operator.
    The purpose is to be able to specify PersistentVolumeClaim,
    Secret, ConfigMap and mount it to the test pod spawned by the
    test-operator.
    
    Depends-On: https://github.com/openstack-k8s-operators/ci-framework/pull/2792

--- pkg/tobiko/const.go
+++ pkg/tobiko/const.go
@@ -1,6 +1,16 @@
 package tobiko
 
+import (
+	"github.com/openstack-k8s-operators/lib-common/modules/storage"
+)
+
 const (
 	// ServiceName - tempest service name
 	ServiceName = "tobiko"
+
+	// Tobiko is the definition of the tobiko group
+	Tobiko storage.PropagationType = "Tobiko"
 )
+
+// TobikoPropagation is the definition of the Tobiko propagation service
+var TobikoPropagation = []storage.PropagationType{Tobiko}
