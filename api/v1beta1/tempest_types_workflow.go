e5900a84dd1a974b464b33d709677f4e6ceb6705
Szymon Datko <sdatko@redhat.com>
2025-08-21 16:57:45 +0200

    [tempest re-run] Add support for new feature
    The tempest runner script has recently got a feature
    that allows re-running the failed tests – for having
    additional insight on the issues repeatability.
    This commit adds the support of enabling that feature
    from the test-operator perspective.
    
    Related: https://github.com/openstack-k8s-operators/tcib/pull/320

--- api/v1beta1/tempest_types_workflow.go
+++ api/v1beta1/tempest_types_workflow.go
@@ -255,6 +255,21 @@ type WorkflowTempestSpec struct {
 
 	// +kubebuilder:validation:Optional
 	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	// +kubebuilder:default:=false
+	// Activate tempest re-run feature. When activated, tempest will perform
+	// another run of the tests that failed during the first execution.
+	RerunFailedTests bool `json:"rerunFailedTests"`
+
+	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	// +kubebuilder:default:=false
+	// Allow override of exit status with the tempest re-run feature.
+	// When activated, the original return value of the tempest run will be
+	// overridden with a result of the tempest run on the set of failed tests.
+	RerunOverrideStatus bool `json:"rerunOverrideStatus"`
+
+	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	// NetworkAttachments is a list of NetworkAttachment resource names to expose
 	// the services to the given network
 	NetworkAttachments *[]string `json:"networkAttachments,omitempty"`
